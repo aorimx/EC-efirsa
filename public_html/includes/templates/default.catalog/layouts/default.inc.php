0c7d8a41d506971e1d8b0aa3b68533a5cbe8a337
inigo98 <navarretigo12@gmail.com>
2017-12-13 10:46:40 -0600

    we-inigo
    footer sin imagenes

--- public_html/includes/templates/default.catalog/layouts/default.inc.php
+++ public_html/includes/templates/default.catalog/layouts/default.inc.php
@@ -8,9 +8,20 @@
 <!--[if lt IE 9]><script src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script><![endif]-->
 <!--[if lt IE 9]><script src="//cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.min.js"></script><![endif]-->
 {snippet:head_tags}
-<link rel="stylesheet" href="{snippet:template_path}css/framework.min.css" />
-<link rel="stylesheet" href="{snippet:template_path}css/app.min.css" />
+  <link rel="stylesheet" href="{snippet:template_path}css/framework.min.css" />
+  <link rel="stylesheet" href="{snippet:template_path}css/app.min.css" />
+  <link rel="stylesheet" href="{snippet:template_path}ef-s/css/normalize.css" />
+  <link rel="stylesheet" href="{snippet:template_path}ef-s/css/bootstrap.css">
+  <link rel="stylesheet" href="{snippet:template_path}ef-s/css/font-awesome.min.css">
+  <link rel="stylesheet" href="{snippet:template_path}ef-s/css/certificaciones.css">
+  <link rel="stylesheet" href="{snippet:template_path}ef-s/css/inicio.css">
+  <link rel="stylesheet" href="{snippet:template_path}ef-s/css/mobileMenu.css">
+  <link rel="stylesheet" href="{snippet:template_path}ef-s/css/nosotros.css">
+  <link rel="stylesheet" href="{snippet:template_path}ef-s/css/contacto.css">
+  <link rel="stylesheet" href="{snippet:template_path}ef-s/css/estilos.css">
+  <link rel="stylesheet" href="{snippet:template_path}ef-s/css/style1.css">
 {snippet:style}
+
 </head>
 <body>
 
@@ -48,7 +59,7 @@
     </div>
   </div>
 
-  <?php include vmod::check(FS_DIR_HTTP_ROOT . WS_DIR_BOXES . 'box_site_footer.inc.php'); ?>
+  <?php include vmod::check(FS_DIR_HTTP_ROOT . WS_DIR_TEMPLATE . '/ef-s/footer.php'); ?>
 </div>
 
 <a id="scroll-up" href="#">
