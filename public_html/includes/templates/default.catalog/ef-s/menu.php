3d59178de1358d33620ba07355d991c573c4a19b
inigo98 <navarretigo12@gmail.com>
2017-12-14 12:58:14 -0600

    we-inigo
    quito border radius del menu
    se le agrego padding al footer

--- public_html/includes/templates/default.catalog/ef-s/menu.php
+++ public_html/includes/templates/default.catalog/ef-s/menu.php
@@ -27,7 +27,7 @@
           <div id="navbar" class="navbar-collapse collapse" style="overflow: hidden; max-height: 424px !important;">
             <ul itemscope itemtype="http://www.schema.org/SiteNavigationElement" class="nav navbar-nav navbar-cel">
               <li>
-                <a class="navbar-brand marca-nocel hidden-xs hidden-sm" href="index.php" style="padding-bottom: 6px; padding-top: 7px; margin-right:0px;"><img src="img/logos/efirsaNav.png"></a>
+                <a class="navbar-brand marca-nocel hidden-xs hidden-sm" href="index.php" style="padding-bottom: 6px; padding-top: 6px; margin-right:0px;"><img src="img/logos/efirsaNav.png"></a>
               </li>
               <li class="arrow_box"><a itemprop="url" href="index.php"><span itemprop="name">Inicio</span></a></li>
               <li class="arrow_box"><a itemprop="url" href="nosotros.php"><span itemprop="name">Nosotros</span></a></li>
