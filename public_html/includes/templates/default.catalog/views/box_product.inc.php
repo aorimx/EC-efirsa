f56cf6ba6847b86696da76f9c8ea94f2179c8902
Andrea_Abundis <andrea.abundis@aori.mx>
2017-12-20 13:13:55 -0600

    release
    boton producto

--- public_html/includes/templates/default.catalog/views/box_product.inc.php
+++ public_html/includes/templates/default.catalog/views/box_product.inc.php
@@ -99,7 +99,7 @@
       </div>
 
 
-      <div class="buy_now" style="margin: 1em 0;">
+      <div class="buy_now">
         <?php echo functions::form_draw_form_begin('buy_now_form', 'post'); ?>
         <?php echo functions::form_draw_hidden_field('product_id', $product_id); ?>
 
