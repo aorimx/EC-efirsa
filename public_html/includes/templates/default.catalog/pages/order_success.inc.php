0c003c97397ca37560bf4deaf0f604a7b4827ae1
Andrea_Abundis <andrea.abundis@aori.mx>
2018-01-08 13:43:10 -0600

    we-andrea
    order success

--- public_html/includes/templates/default.catalog/pages/order_success.inc.php
+++ public_html/includes/templates/default.catalog/pages/order_success.inc.php
@@ -1,14 +1,13 @@
-<main id="content">
+<main id="content" class="container-alert">
   {snippet:notices}
 
-  <div id="box-order-success" class="box">
-
+  <div id="box-order-success" class="order-success">
+	<i class="fa fa-cart-plus" aria-hidden="true"></i>
     <h1 class="title"><?php echo strtr(language::translate('title_order_completed', 'Your order %order_id is successfully completed!'), array('%order_id' => $order['id'])); ?></h1>
 
     <p><?php echo language::translate('description_order_completed', 'Thank you for shopping in our store. An order confirmation e-mail has been sent. We will process your order shortly.'); ?></p>
-
-    <p style="padding-left: 24px;"><a href="<?php echo htmlspecialchars($printable_link); ?>" target="_blank"><?php echo language::translate('description_click_printable_copy', 'Click here for a printable copy'); ?></a></p>
-
+    <button class="btn-print" style="padding-left: 24px;"><a href="<?php echo htmlspecialchars($printable_link); ?>" target="_blank"><?php echo language::translate('description_click_printable_copy', 'Click here for a printable copy'); ?></a></button>
+    <button> <a href="../../index.php">Seguir comprando</a></button>
     <?php if ($payment_receipt) echo $payment_receipt; ?>
 
     <?php if ($order_success_modules_output) echo $order_success_modules_output; ?>
