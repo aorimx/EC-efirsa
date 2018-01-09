c62479554f7b3fd6b77a84972bb4cd48047d974a
LeoOliva <fenix_de_plata@hotmail.com>
2018-01-08 18:09:04 -0600

    Creación de template base
    Se ha creado:
    -Una carpeta como template de Efirsa, en la cual se centralizará todos los cambios
    -Se ha modificado lo enlaces del footer y header
    -Se ha unificado el footer y header con Litecart para evitar tener repetido (Litecart y e desarrollo EF-S en PHP)
    -Se ha incorporado y modificado las páginas previamente desarrolladas en PHP (EF-S) a Litecart
    -Se han movido los recursos del repo EF-S a la template

---
+++ public_html/pages/contacto.inc.php
@@ -0,0 +1,12 @@
+<?php
+  document::$snippets['title'][] = 'Contacto';
+  document::$snippets['description'] = 'Your meta description here';
+ 
+  $_mypage = new view();
+ 
+  $_mypage->snippets = array(
+    //'title' => 'Hello World',
+  );
+ 
+  echo $_mypage->stitch('pages/contacto');
+?>
