62c9780363d7699ca450572ad7faa763d275b103
inigo98 <navarretigo12@gmail.com>
2018-01-15 12:56:20 -0600

    we-inigo
    quitar todos los estilos menos sagan

--- public_html/includes/templates/efirsa.catalog/layouts/default.inc.php
+++ public_html/includes/templates/efirsa.catalog/layouts/default.inc.php
@@ -7,18 +7,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <!--[if lt IE 9]><script src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script><![endif]-->
 <!--[if lt IE 9]><script src="//cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.min.js"></script><![endif]-->
-{snippet:head_tags}
-<link rel="stylesheet" href="{snippet:template_path}css/normalize.css" />
-  <link rel="stylesheet" href="{snippet:template_path}css/bootstrap.css">
-  <link rel="stylesheet" href="{snippet:template_path}css/font-awesome.min.css">
-  <link rel="stylesheet" href="{snippet:template_path}css/certificaciones.css">
-  <link rel="stylesheet" href="{snippet:template_path}css/inicio.css">
-  <link rel="stylesheet" href="{snippet:template_path}css/mobileMenu.css">
-  <link rel="stylesheet" href="{snippet:template_path}css/nosotros.css">
-  <link rel="stylesheet" href="{snippet:template_path}css/estilos.css">
-  <link rel="stylesheet" href="{snippet:template_path}css/style1.css">
   <link href="{snippet:template_path}Sagan-boilerplate/assets/styles/main.css" rel="stylesheet">
-{snippet:style}
 </head>
 <body>
 
