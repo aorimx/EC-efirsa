2feb68edd9ac178a02860a7117cde3f102bdc8b1
Leonardo Oliva <fenix_de_plata@hotmail.com>
2018-01-16 11:56:06 -0600

    Barra Order by

--- public_html/includes/templates/efirsa.catalog/pages/search_results.inc.php
+++ public_html/includes/templates/efirsa.catalog/pages/search_results.inc.php
@@ -8,14 +8,25 @@
     <div class="btn-group pull-right hidden-xs container-filtros">
 <?php
   $separator = false;
+  echo '<select id="bar-order-by">';
   foreach ($sort_alternatives as $key => $value) {
     if ($_GET['sort'] == $key) {
-      echo '<span class="btn btn-default activo">'. $value .'</span>';
+      //echo '<span class="btn btn-default activo">'. $value .'</span>';
+      echo '<option selected>' . $value  . '</option>';
     } else {
-      echo '<a class="btn btn-default" href="'. document::href_ilink(null, array('sort' => $key), true) .'">'. $value .'</a>';
+      //echo '<a class="btn btn-default" href="'. document::href_ilink(null, array('sort' => $key), true) .'">'. $value .'</a>';
+      echo '<option value="' . document::href_ilink(null, array('sort' => $key), true) .  '">' . $key . '</option>';
     }
   }
+  echo '</select>';
 ?>
+<img id="bar-order-by-show-grid" alt="Celdas"/>
+<img id="bar-order-by-show-list" alt="List" />
+
+
+<div class="toggle list">TOGGLE</div>
+
+
     </div>
   <div class="sub-menu_search">  
     {snippet:breadcrumbs}
