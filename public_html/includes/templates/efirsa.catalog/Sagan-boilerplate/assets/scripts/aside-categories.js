3d0df7bdc0ad7379d87596f599383f3142dec312
Andrea_Abundis <andrea.abundis@aori.mx>
2018-01-16 12:50:15 -0600

    release
    avance footer

--- public_html/includes/templates/efirsa.catalog/Sagan-boilerplate/assets/scripts/aside-categories.js
+++ public_html/includes/templates/efirsa.catalog/Sagan-boilerplate/assets/scripts/aside-categories.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {   
        $('.btn-categories').on('click', function () {
            $('.c-oculto').toggleClass('c-mostrar');
-           $('.flip-right').toggleClass('icon-down');
-           $('.padding-le').toggleClass('no-padding');
+           /*$('.flip-right').toggleClass('icon-down');
+           $('.padding-le').toggleClass('no-padding');*/
        });
    });
