53fc46b40c14b1708929c915295809470a2c367c
Andrea_Abundis <andrea.abundis@aori.mx>
2018-01-19 13:57:48 -0600

    we-andrea
    manufacture

--- public_html/includes/templates/efirsa.catalog/views/box_manufacturer_logotypes.inc.php
+++ public_html/includes/templates/efirsa.catalog/views/box_manufacturer_logotypes.inc.php
@@ -1,10 +1,10 @@
-<div id="box-manufacturer-logotypes" class="box hidden-xs hidden-sm">
+<div id="box-manufacturer-logotypes" class="box hidden-xs hidden-sm box-manufacturer">
 	<button class="title btn-categories">Manufacturer</button>
   <ul class="list-inline text-center">
-  	<div style="display: inline-flex;">
+  	<div class="box-manufaturer-img">
     <?php foreach ($logotypes as $logotype) { ?>
       <a href="<?php echo htmlspecialchars($logotype['link']); ?>">
-        <img src="<?php echo htmlspecialchars($logotype['image']); ?>" alt="" title="<?php echo htmlspecialchars($logotype['title']); ?>" style="margin: 0px 15px;">
+        <img class="manufacturer-image" src="<?php echo htmlspecialchars($logotype['image']); ?>" alt="" title="<?php echo htmlspecialchars($logotype['title']); ?>" style="margin: 0px 15px;">
       </a>
     <?php } ?>
 </div>
