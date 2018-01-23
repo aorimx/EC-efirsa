7e246f25abee2bdff543c856a86410d02cfecf65
inigo98 <navarretigo12@gmail.com>
2018-01-23 11:07:23 -0600

    we-inigo
    arreglar error del app js

--- public_html/includes/templates/efirsa.catalog/layouts/checkout.inc.php
+++ public_html/includes/templates/efirsa.catalog/layouts/checkout.inc.php
@@ -5,11 +5,10 @@
 <meta charset="{snippet:charset}" />
 <meta name="description" content="{snippet:description}" />
 <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="{snippet:template_path}css/font-awesome.min.css">
-  <link rel="stylesheet" href="{snippet:template_path}Sagan-boilerplate/assets/styles/main.css">
-<link rel="stylesheet" type="text/css" href="{snippet:template_path}Sagan-boilerplate/assets/slick/slick.css"/>
-<link rel="stylesheet" type="text/css" href="{snippet:template_path}Sagan-boilerplate/assets/slick/slick-theme.css"/>
-<script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.2.1.min.js"></script>
+<link rel="stylesheet" href="{snippet:template_path}css/font-awesome.min.css">
+<link rel="stylesheet" href="{snippet:template_path}Sagan-boilerplate/assets/styles/main.css">
+<link rel="stylesheet" href="{snippet:template_path}Sagan-boilerplate/assets/slick/slick.css"/>
+<link rel="stylesheet" href="{snippet:template_path}Sagan-boilerplate/assets/slick/slick-theme.css"/>
 </head>
 <body>
     <header id="header">
@@ -37,18 +36,12 @@
   </div>
 {snippet:foot_tags}
 <script src="{snippet:template_path}js/app.min.js"></script>
-<script src="{snippet:template_path}js/jquery.js"></script>
 <script src="{snippet:template_path}js/mobilMenu.js"></script>
-<script src="{snippet:template_path}js/bootstrap.min.js"></script>
-<script src="{snippet:template_path}Sagan-boilerplate/assets/scripts/prueba.js"></script>
 <script src="{snippet:template_path}js/scripts.js"></script>
 <script src="{snippet:template_path}Sagan-boilerplate/assets/scripts/checkout.js"></script>
-<script type="text/javascript" src="{snippet:template_path}Sagan-boilerplate/assets/slick-1.8.0/slick/slick.js"></script>
+<script src="{snippet:template_path}js/jquery.js"></script>
 <script src="{snippet:template_path}Sagan-boilerplate/assets/scripts/prueba.js"></script>
-
-<script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
-<script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
-<script type="text/javascript" src="{snippet:template_path}Sagan-boilerplate/assets/slick/slick.min.js"></script>
+<script src="{snippet:template_path}Sagan-boilerplate/assets/slick/slick.min.js"></script>
 {snippet:javascript}
 </body>
 </html>
--- public_html/includes/templates/efirsa.catalog/layouts/default.inc.php
+++ public_html/includes/templates/efirsa.catalog/layouts/default.inc.php
@@ -7,27 +7,21 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <!--[if lt IE 9]><script src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script><![endif]-->
 <!--[if lt IE 9]><script src="//cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.min.js"></script><![endif]-->
-<script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.2.1.min.js"></script>
-  <link rel="stylesheet" href="{snippet:template_path}css/font-awesome.min.css">
-  <link href="{snippet:template_path}Sagan-boilerplate/assets/styles/main.css" rel="stylesheet">
-<link rel="stylesheet" type="text/css" href="{snippet:template_path}Sagan-boilerplate/assets/slick/slick.css"/>
-<link rel="stylesheet" type="text/css" href="{snippet:template_path}Sagan-boilerplate/assets/slick/slick-theme.css"/>
+<link rel="stylesheet" href="{snippet:template_path}css/font-awesome.min.css">
+<link rel="stylesheet" href="{snippet:template_path}Sagan-boilerplate/assets/styles/main.css">
+<link rel="stylesheet" href="{snippet:template_path}Sagan-boilerplate/assets/slick/slick.css"/>
+<link rel="stylesheet" href="{snippet:template_path}Sagan-boilerplate/assets/slick/slick-theme.css"/>
 </head>
 <body>
-
 <div id="page">
   <header>
     <div id="region-wrapper">
       <!--<?php //include vmod::check(FS_DIR_HTTP_ROOT . WS_DIR_BOXES . 'box_region.inc.php'); ?>-->
     </div>
-
     <?php include vmod::check(FS_DIR_HTTP_ROOT . WS_DIR_BOXES . 'box_cart.inc.php'); ?>
-
     <?php include vmod::check(FS_DIR_HTTP_ROOT . WS_DIR_TEMPLATE . '/layouts/menu.php'); ?>
   </header>
-
   <!--<?php //include vmod::check(FS_DIR_HTTP_ROOT . WS_DIR_BOXES . 'box_site_menu.inc.php'); ?>-->
-  
     <main class="j-workspace ">
       <div class="top">
         {snippet:top}
@@ -41,33 +35,23 @@
       <div class="bottom">
         {snippet:bottom}
       </div>
-
-     
     </main>
      <?php include vmod::check(FS_DIR_HTTP_ROOT . WS_DIR_TEMPLATE . '/layouts/footer.php'); ?>
 </div>
-
-
 <!--<a id="scroll-up" href="#">
   <?php //echo functions::draw_fonticon('fa-chevron-circle-up fa-3x', 'style="color: #000;"'); ?>
 </a>-->
-
 {snippet:foot_tags}
 <script src="{snippet:template_path}js/app.min.js"></script>
-<script src="{snippet:template_path}js/jquery.js"></script>
 <script src="{snippet:template_path}js/mobilMenu.js"></script>
-<script src="{snippet:template_path}js/bootstrap.min.js"></script>
 <script src="{snippet:template_path}js/scripts.js"></script>
 <script src="{snippet:template_path}js/bar-order-by.js"></script>
-<script src="{snippet:template_path}Sagan-boilerplate/assets/scripts/aside-categories.js"></script>
-<script src="{snippet:template_path}Sagan-boilerplate/assets/scripts/prueba.js"></script>
 <script src="{snippet:template_path}js/sweetalert2/dist/sweetalert2.all.min.js"></script>
-<script type="text/javascript" src="{snippet:template_path}Sagan-boilerplate/assets/slick-1.8.0/slick/slick.js"></script>
+<script src="{snippet:template_path}Sagan-boilerplate/assets/scripts/aside-categories.js"></script>
 
+<script src="{snippet:template_path}js/jquery.js"></script>
+<script src="{snippet:template_path}Sagan-boilerplate/assets/slick/slick.min.js"></script>
 <script src="{snippet:template_path}Sagan-boilerplate/assets/scripts/prueba.js"></script>
-<script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
-<script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
-<script type="text/javascript" src="{snippet:template_path}Sagan-boilerplate/assets/slick/slick.min.js"></script>
 {snippet:javascript}
 </body>
 </html>
