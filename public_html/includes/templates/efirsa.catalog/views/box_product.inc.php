2e9d48933a355ee85f99ec8d4725f9b7b13cccf5
AndreaPAbundis <lizbethabundispgmail.com>
2018-01-23 12:34:17 -0600

    we-andrea
    options group en producto

--- public_html/includes/templates/efirsa.catalog/views/box_product.inc.php
+++ public_html/includes/templates/efirsa.catalog/views/box_product.inc.php
@@ -66,7 +66,7 @@
       </div>
       <?php } ?>
 
-    -->    
+    -->
 
         </div>
 
@@ -82,8 +82,8 @@
 
     <div class="col-sm-halfs col-md-thirds">
 
-     
-   
+
+
 
 
       <?php if ($cheapest_shipping_fee !== null) { ?>
@@ -129,8 +129,10 @@
           <?php foreach ($options as $option) { ?>
           <div class="form-group">
             <label><?php echo $option['name']; ?></label>
+            <div class="select-product">
             <?php echo $option['description'] ? '<div>' . $option['description'] . '</div>' : ''; ?>
             <?php echo $option['values']; ?>
+          </div>
           </div>
           <?php } ?>
         <?php } ?>
@@ -155,7 +157,7 @@
         <?php echo functions::form_draw_form_end(); ?>
       </div>
 
-      
+
       <div class="social-bookmarks text-center" style="margin: 1rem 0;">
         <a class="link" href="#"><?php echo functions::draw_fonticon('fa-link', 'style="color: #333;"'); ?></a>
         <a class="twitter" href="<?php echo document::href_link('http://twitter.com/home/', array('status' => $name .' - '. $link)); ?>" target="_blank" title="<?php echo sprintf(language::translate('text_share_on_s', 'Share on %s'), 'Twitter'); ?>"><?php echo functions::draw_fonticon('fa-twitter-square fa-lg', 'style="color: #55acee;"'); ?></a>
@@ -172,7 +174,7 @@
 <hr>
 <div class="grid">
   <div class="unit half">
-  <div class="container-descripcion"> 
+  <div class="container-descripcion">
     <h3> Descripción</h3>
       <?php if ($description) { ?>
         <?php echo $description; ?>
@@ -212,9 +214,9 @@
   <?php }
   else{ ?>
     <p style="color: #000; opacity: .65;">No hay características para este producto</p>
-  <?php }?> 
+  <?php }?>
   <?php } ?>
-  
+
   </div>
 </div>
 </div>
