e5733e6c7445bb21cb4492d79c81f96733cd7ed2
Leonardo Oliva <fenix_de_plata@hotmail.com>
2018-01-24 10:52:15 -0600

    Mensaaje de error al mostar la secciòn de categorias vacias

--- public_html/includes/templates/efirsa.catalog/pages/categories.inc.php
+++ public_html/includes/templates/efirsa.catalog/pages/categories.inc.php
@@ -10,8 +10,12 @@
   <?php include vmod::check(FS_DIR_HTTP_ROOT . WS_DIR_TEMPLATE . 'views/box_search.inc.php'); ?>
   <div id="box-categories" class="box">
     <h1 class="categories-title"><?php echo language::translate('title_categories', 'Categories'); ?></h1>
-    <div class="box-thumbnail">
-      <?php foreach ($categories as $category) echo functions::draw_listing_category($category); ?>
+    <div class="box-thumbnail"> 
+      <?php if(!empty($categories)){ ?>
+        <?php foreach ($categories as $category) echo functions::draw_listing_category($category); ?>
+      <?php }else{ ?>
+        Por el momento no hay categorias que mostrar
+      <?php } ?>
     </div>
   </div>
 </main>
