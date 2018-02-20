40dedafe3da27c08626c537166409e5195f6953d
AndreaPAbundis <lizbethabundispgmail.com>
2018-02-19 18:53:53 -0600

    we-andrea
    Nuevo home proceso

---
+++ public_html/includes/templates/efirsa.catalog/pages/index_landing2.inc.php
@@ -0,0 +1,13 @@
+<main id="content" class="main_content" style="width:100%;">
+  <div class="hero-content">
+    <div class="hero-image">
+      <div class="hero-text">
+        <h1>Lorem ipsum </h1>
+        <p>dolor sit amet</p>
+        <a href="<?php echo $data['btn_link'] ?>"><button>Lorem ipsum</button></a>
+      </div>
+    </div>
+  </div>
+<?php include vmod::check(FS_DIR_HTTP_ROOT . WS_DIR_TEMPLATE . 'views/Componente2.inc.php'); ?>
+<?php include vmod::check(FS_DIR_HTTP_ROOT . WS_DIR_TEMPLATE . 'views/Componente3.inc.php'); ?>
+</main>
---
+++ public_html/includes/templates/efirsa.catalog/views/Componente4.inc.php
@@ -0,0 +1,13 @@
+<div>
+	<div class="content-cita">
+		<div class="content-picture">
+			<img src="https://cambiardeimagen.files.wordpress.com/2013/03/moda-masculina-lentes-cara-hombre-carametria-caramorfoligia-consultoria-de-imagen.jpg?w=646&h=823">
+		</div>
+		<div class="content-info">
+			<h1></h1>
+			<p><strong><em></em></strong></p>
+			<p>fulanito de tal</p>
+			   <a href=""><button></button></a>
+		</div>
+	</div>
+</div>
