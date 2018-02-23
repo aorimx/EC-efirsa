7f55a9eb28ee0c176d9d439a919daf5faa808560
AndreaPAbundis <lizbethabundispgmail.com>
2018-02-22 18:15:41 -0600

    we-andrea
    home e-commerce

--- public_html/includes/templates/efirsa.catalog/pages/index_tienda.inc.php
+++ public_html/includes/templates/efirsa.catalog/pages/index_tienda.inc.php
@@ -30,13 +30,11 @@
       <?php include vmod::check(FS_DIR_HTTP_ROOT . WS_DIR_BOXES . 'box_campaign_products.inc.php'); ?>
     </div>
     <?php } ?>
-    <!--
-    <div class="tab-pane fade in" id="popular-products">
-      <?php //include vmod::check(FS_DIR_HTTP_ROOT . WS_DIR_BOXES . 'box_popular_products.inc.php'); ?>
-    </div>-->
-
     <div class="tab-pane fade in active" id="latest-products">
       <?php include vmod::check(FS_DIR_HTTP_ROOT . WS_DIR_BOXES . 'box_latest_products.inc.php'); ?>
+    </div>
+    <div class="tab-pane fade in" id="popular-products">
+      <?php include vmod::check(FS_DIR_HTTP_ROOT . WS_DIR_BOXES . 'box_popular_products.inc.php'); ?>
     </div>
   </div>
 
--- public_html/includes/templates/efirsa.catalog/views/box_popular_products.inc.php
+++ public_html/includes/templates/efirsa.catalog/views/box_popular_products.inc.php
@@ -1,5 +1,5 @@
 <div id="box-popular-products">
-
+<h1 class="padding-left">Popular products</h1>
   <div class="products row half-gutter text-center box-thumbnail">
     <?php foreach($products as $product) echo functions::draw_listing_product($product); ?>
   </div>
